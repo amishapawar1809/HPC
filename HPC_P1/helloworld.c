64f2e69d5520032d25fa2f76d86e5ce5b746c63b
amishapawar1809 <71626447+amishapawar1809@users.noreply.github.com>
2021-11-21 18:42:06 +0530

    Update helloworld.c

--- HPC_P1/helloworld.c
+++ HPC_P1/helloworld.c
@@ -1,1 +1,18 @@
+//OpenMP program to print Hello World using C language
+
+#include <omp.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	//omp_set_num_threads(8);
+	//Beginning of parallel region
+	#pragma omp parallel
+	{
+		printf("Hello World...from thread = %d\n", omp_get_thread_num());
+	}
+	//Ending of parallel region
+}
 
